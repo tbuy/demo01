c11cc25d8cfef47cbec049485609f510a57ae2c1
wqy <wqy445@gmail.com>
2017-08-06 14:29:21 +0800

    首页

---
+++ src/Detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>22222</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+    }
+  },
+  components:{
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="item">
+    <div class="title" :style="{backgroundColor:itemData.bg}">
+      <span :style="{color: itemData.color}">{{itemData.title}}</span>
+      <i class="iconfont icon-add" @click="add(index)"></i>
+    </div>
+    <div class="content" v-for="i in [1,1,1,1]">
+      <i class="iconfont icon-circle1" :style="{color: itemData.color}"></i>
+      <span>完成</span>
+      <span class="time">08/03 10:00</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    itemData:{
+      type: Array,
+      default: []
+    },
+    index:{
+      type:Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods:{
+    add: function(index){
+      console.log(index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .title{
+    height: 50px;
+    line-height: 50px;
+    padding: 0 10px;
+    box-sizing: border-box;
+    width: 100%;
+  }
+  .title span{
+    font-size: 20px;
+    color:red;
+  }
+  .title i{
+    font-size: 20px!important;
+    vertical-align: middle;
+    color: #999;
+    float: right;
+  }
+  .content {
+    font-size: 16px;
+    height: 40px;
+    line-height: 40px;
+    padding: 0 10px;
+    box-sizing: border-box;
+    border-bottom: 1px dashed #ccc;
+  }
+  .content i {
+    font-size: 24px;
+    margin-left: 10px;
+    vertical-align: middle;
+  }
+  .content .time{
+    float: right;
+    color: red;
+    margin-right: 10px;
+  }
+</style>
